37a04ccecda24cccedaeefe5d45a8b659e651fd0
Denzell Gibson <19319491+denzellgibson@users.noreply.github.com>
2024-11-03 19:18:34 -0500

    Add round logic and results display.

--- game.js
+++ game.js
@@ -4,6 +4,62 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
   }
 
+const playerChoiceLabel = document.querySelector('#PlayerChoiceLabel');
+const computerChoiceLabel = document.querySelector('#ComputerChoiceLabel');
+const roundResultLabel = document.querySelector('#RoundResultLabel');
+const playerScoreLabel = document.querySelector('#PlayerScoreLabel');
+const computerScoreLabel = document.querySelector('#ComputerScoreLabel');
+const RockButton = document.querySelector("#RockButton");
+const PaperButton = document.querySelector("#PaperButton");
+const ScissorsButton = document.querySelector("#ScissorsButton");
+
+let playerScore = 0;
+let computerScore = 0;
+
+RockButton.addEventListener("click", (e) => {
+    playerRound('rock');
+});
+PaperButton.addEventListener("click", (e) => {
+    playerRound('paper');
+});
+ScissorsButton.addEventListener("click", (e) => {
+    playerRound('scissors');
+});
+
+function playerRound(userChoice) {
+    let computerChoice = getComputerChoice();
+    console.log('Computer Choice: ' + computerChoice);
+    console.log('Player Choice: ' + userChoice);
+
+    if (userChoice == computerChoice) {
+        updateResultsDisplay("ties");
+    }
+    else if (userChoice == "rock" && computerChoice == "paper") {
+        computerScore++;
+        updateResultsDisplay('loses');
+    }
+    else if (userChoice == 'rock' && computerChoice == 'scissors') {
+        playerScore++;
+        updateResultsDisplay('wins');
+    }
+    else if (userChoice == 'paper' && computerChoice == 'rock') {
+        playerScore++;
+        updateResultsDisplay('wins');
+    }
+    else if (userChoice == 'paper' && computerChoice == 'scissors') {
+        computerScore++;
+        updateResultsDisplay('loses');
+    }
+    else if (userChoice == 'scissors' && computerChoice == 'rock') {
+        computerScore++;
+        updateResultsDisplay('loses');
+    }
+    else if (userChoice == 'scissors' && computerChoice == 'paper') {
+        playerScore++;
+        updateResultsDisplay('wins');
+    }
+}
+
 function getComputerChoice() {
     let randomNumber = getRandomIntInclusive(1,3);
     if (randomNumber == 1) {
@@ -17,52 +73,24 @@ function getComputerChoice() {
     }
 }
 
-function outputResult(userChoice, computerChoice, result) {
-    alert(userChoice + " vs. " + computerChoice + ". User " + result + "! \n User Score: " + humanScore + "\n Computer Score: " + computerScore);
+function updateResultsDisplay(outcome) {
+    playerScoreLabel.innerHTML = 'Player Score: ' + playerScore;
+    computerScoreLabel.innerHTML = 'Computer Score: ' + computerScore;
+    roundResultLabel.innerHTML = 'Round Result: ' + outcome;
 }
 
-function playerRound(userChoice, computerChoice) {
-    if (userChoice == computerChoice) {
-        outputResult(userChoice, computerChoice, "ties");
-    }
-    else if (userChoice == "rock" && computerChoice == "paper") {
-        computerScore++;
-        outputResult(userChoice, computerChoice, "loses");
-    }
-    else if (userChoice == 'rock' && computerChoice == 'scissors') {
-        humanScore++;
-        outputResult(userChoice, computerChoice, 'wins');
-    }
-    else if (userChoice == 'paper' && computerChoice == 'rock') {
-        humanScore++;
-        outputResult(userChoice, computerChoice, 'wins');
-    }
-    else if (userChoice == 'paper' && computerChoice == 'scissors') {
-        computerScore++;
-        outputResult(userChoice, computerChoice, 'loses');
-    }
-    else if (userChoice == 'scissors' && computerChoice == 'rock') {
-        computerScore++;
-        outputResult(userChoice, computerChoice, 'loses');
-    }
-    else if (userChoice == 'scissors' && computerChoice == 'paper') {
-        humanScore++;
-        outputResult(userChoice, computerChoice, 'wins');
-    }
-    else {
-        alert("Not a valid input");
-    }
-}
 
-let userChoice, computerChoice;
+
+/* let userChoice, computerChoice;
 let humanScore = 0, computerScore = 0;
-let numGames = 0;
+let numGames = 0; */
 
-numGames = parseInt(prompt('How many games do you want to play?'));
 
-for (i = 1; i <= numGames; i++) {
+
+/* for (i = 1; i <= numGames; i++) {
     userChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
     computerChoice = getComputerChoice();
     playerRound(userChoice, computerChoice);
-}
+} */
+
 
